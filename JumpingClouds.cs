631e85c2db4958d9a8670d36cf2aecd3cef1b113
Oscar <Oscarponteuk@gmail.com>
2020-05-20 15:49:28 +0100

    Adding Projects to GitHub

---
+++ JumpingClouds.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace JumpingClouds
+{
+    public class JumpingClouds
+    {
+        public int JumpingOnClouds(string cloud)
+        {
+            var c_temp = cloud.Split(' ');
+            var clouds = Array.ConvertAll(c_temp, int.Parse);
+            var position = 0;
+            var jumpCount = 0;
+
+            while (true)
+            {
+                if (position + 2 <= clouds.Length - 1 && clouds[position + 2] == 0)
+                    position = position + 2;
+                else
+                    position++;
+
+                jumpCount++;
+                if (position == clouds.Length - 1)
+                    break;
+            }
+            return jumpCount;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpingClouds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert few clouds separated by space ' ' ex: '1 0 0 1 1 0 0' ");
+
+            var input = Console.ReadLine();
+            var jumps = new JumpingClouds();
+            
+            Console.WriteLine("Number of jumps to win: " + jumps.JumpingOnClouds(input));
+        }
+
+    }
+}
